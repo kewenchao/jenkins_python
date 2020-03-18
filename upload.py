785b95ef3d7feefc8757fd2bf8217638db5449c6
kewenchao <1095527089@qq.com>
2020-03-18 02:03:10 -0700

    Update upload.py

--- upload.py
+++ upload.py
@@ -1,8 +1,5 @@
 import os
 def test():
-    envs=os.getenv("TEST_ENV")
-    print("yes,youget first" + envs)
-    name="test"+envs
-    print(111111111111)
-    return name
+    os.environ['TEST_ENV']="ready go"
+    print("upload yes")
 test()
