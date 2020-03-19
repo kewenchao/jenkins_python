e47356323ba068434fd0427976a4adc688967d77
kewenchao <1095527089@qq.com>
2020-03-18 20:18:44 -0700

    Update upload.py

--- upload.py
+++ upload.py
@@ -4,7 +4,7 @@ def test():
     a=os.environ
     print(a)
     TEST_ENV="22222222222222222"
-    os.environ.setdefault('NEW_ENV') = "111111111111111111111111111111"
+    os.putenv('NEW_ENV',"11111111111111111111")
     print("upload yes")
    
 test()
