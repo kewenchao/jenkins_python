c3c4f3b1579b3ad44c3a67397a1c44c51334b79a
kewenchao <1095527089@qq.com>
2020-03-18 19:35:15 -0700

    Update upload.py

--- upload.py
+++ upload.py
@@ -1,7 +1,7 @@
 import os
 def test():
     print("yes , you are")
-    myfile=open("/home/keke/jenkins/workspace/test--inject","w")
+    myfile=open("/home/keke/jenkins/workspace/test--inject/env.properties","w")
     TEST_ENV="22222222222222222"
     print("upload yes")
     myfile.close()
