fbd591838d9a6cef02716c43ccf447605224c88e
kewenchao <1095527089@qq.com>
2020-03-18 22:47:31 -0700

    Update upload.py

--- upload.py
+++ upload.py
@@ -4,7 +4,8 @@ def test():
     a=os.environ
     print(a)
     TEST_ENV="22222222222222222"
-    os.environ['PWD'] = "/home/keke"
+    print(1111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111)
+    os.environ['HEHEHE'] = "000000000000000000"
     b=os.environ
     print(b)
     print("upload yes")
